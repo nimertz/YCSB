4f353b9278e05f9a99af52accc22e2e2cad58461
Nikolaj Mertz <nikolajmertz@gmail.com>
2021-09-12 23:06:25 +0200

    New skew generator

--- core/src/main/java/site/ycsb/generator/SkewGenerator.java
+++ core/src/main/java/site/ycsb/generator/SkewGenerator.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. Copyright (c) 2017 YCSB contributors. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+
 package site.ycsb.generator;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -5,6 +22,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class SkewGenerator extends NumberGenerator {
   private final long lb, ub, interval;
 
+  /**
+   * Roll Twice, Pick Better
+   */
   public SkewGenerator(long lb, long ub) {
     this.lb = lb;
     this.ub = ub;
@@ -17,7 +37,7 @@ public class SkewGenerator extends NumberGenerator {
     long first = Math.abs(ThreadLocalRandom.current().nextLong()) % interval  + lb;
     long second = Math.abs(ThreadLocalRandom.current().nextLong()) % interval  + lb;
 
-    if( first > second ) {
+    if(first > second) {
       setLastValue(first);
       return first;
     } else {
